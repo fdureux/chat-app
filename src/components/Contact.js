a1b25a823caa68b26214af2271c80000f1e7909f
fdureux <fanny.dureux@gmail.com>
2020-10-08 11:08:37 +0200

    create Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,23 @@
 import React from 'react';
-import Contact from '../styles/Contact.css';
+import '../styles/Contact.css';
+
+
+const name = 'Nora Anderson';
+const avatar = 'https://randomuser.me/api/portraits/women/65.jpg';
+const online = false;
 
 function Contact() {
-
+    return ( <div className="Contact">
+        <img className="avatar" src={avatar} alt='profile'/>
+        <div>
+            <div className="name">{name}</div>
+            <div className="status">
+                {online ? <div className="status-online"></div>: <div className="status-offline"></div> } 
+                <div className="status-text">{online? 'Online' : 'Offline'} </div>
+            </div>
+            </div>
+        </div>
+    );
 }
+ 
+export default Contact;
